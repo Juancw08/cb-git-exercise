fa4b60e9e77671e9718960ea56bc64b3ffc7de31
TheGoldenKyle <kylegolden@berkeley.edu>
2020-07-30 01:09:01 -0700

    Function Four from func_4_branch and Function Five

--- git_exercise.py
+++ git_exercise.py
@@ -7,3 +7,12 @@ def function_two():
 def function_three():
     print("Function Three from working_branch")
 
+def function_four():
+    print("Function Four from func_4_branch")
+
+def function_five():
+<<<<<<< HEAD
+	print("Function 5")
+=======
+    print("Function Five :)")
+>>>>>>> 6c153b3... Function Five
